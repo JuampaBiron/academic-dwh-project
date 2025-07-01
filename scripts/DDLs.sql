8212c8fa697028f4613f054add7d1f8331a8873a
juampa biron <85587142+JuampaBiron@users.noreply.github.com>
2025-07-01 18:40:14 -0400

    Create DDLs.sql

---
+++ scripts/DDLs.sql
@@ -0,0 +1,11 @@
+CREATE TABLE sch_bronze.pfl_publicaciones(
+	ID_Academico INT,
+	Nombre_Academico NVARCHAR(100),
+	Autores NVARCHAR(200),
+	Título NVARCHAR(MAX),
+	Año INT,
+	Revista NVARCHAR(250),
+	Tipo NVARCHAR (100),
+	Link NVARCHAR(200),
+	DOI NVARCHAR(100)
+);
